18b346e929f6a831d8036f3fff5b63467784cf48
Candace <2100978@talnet.nl>
2020-02-28 12:45:50 +0100

    Uitwerking van de exercise

---
+++ forum_registratie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registratie</title>
+</head>
+
+<body>
+
+
+    <form action="registratie_handler.php" method="POST">
+        <label>
+            <input type="text" placeholder="Username...." name="username"> Username
+        </label>
+
+        <label>
+            <input type="text" placeholder="Email...." name="email"> Email
+        </label>
+
+        <label>
+            <input type="text" placeholder="Age....." name="age"> Age
+        </label>
+
+        <label>
+            <input type="submit" value="Submit">
+        </label>
+
+    </form>
+</body>
+
+</html>
+
+<?php
---
+++ registratie_handler.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Handler</title>
+</head>
+
+<body>
+    Jouw username is <?php echo $_POST["username"]; ?> <br>
+    Jouw email is <?php echo $_POST["email"]; ?><br>
+    Jouw age is <?php echo $_POST["age"]; ?><br>
+</body>
+
+</html>
